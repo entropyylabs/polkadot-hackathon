56fc12caf80e3eca67e9228e31f945909d239452
Ayush Jain <ayushjain2205@gmail.com>
2023-02-16 12:01:28 +0530

    init camera view component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Cameraview from "../components/Cameraview";
 
 const Landing = () => {
   return (
     <div>
       <h1>Landing</h1>
+      <Cameraview />
     </div>
   );
 };
