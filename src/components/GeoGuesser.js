9b7ae04c1d0630e1e5b1c29d4aebff3fc30ffcff
Ayush Jain <ayushjain2205@gmail.com>
2023-02-17 00:21:35 +0530

    Update geoguesser clue

--- src/components/GeoGuesser.js
+++ src/components/GeoGuesser.js
@@ -2,19 +2,23 @@ import React from "react";
 import ReactStreetview from "react-streetview";
 
 const GeoGuesser = () => {
+  // const googleMapsApiKey = "AIzaSyC9TTEQVKFDvpZkwwqqOOWQu7SD1rLbE7Q";
+
   const googleMapsApiKey = process.env.REACT_APP_MAPS_KEY;
+
+  console.log(googleMapsApiKey);
 
   // see https://developers.google.com/maps/documentation/javascript/3.exp/reference#StreetViewPanoramaOptions
   const streetViewPanoramaOptions = {
-    position: { lat: 46.9171876, lng: 17.8951832 },
+    position: { lat: 48.86750220793768, lng: 2.78353936022087 },
     pov: { heading: 100, pitch: 0 },
     zoom: 1,
   };
   return (
     <div
       style={{
-        width: "800px",
-        height: "450px",
+        width: "390px",
+        height: "840px",
         backgroundColor: "#eeeeee",
       }}
     >
