e0f07b2dae40d0654e5d022449a493bc0e47b0d3
Ayush Jain <ayushjain2205@gmail.com>
2023-02-17 00:22:11 +0530

    Configure mobile only view for website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import useWindowDimensions from "./utils/useWindowDimensions";
 import LandingPage from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
@@ -9,23 +10,33 @@ import Celebration from "./pages/Celebration";
 import Word from "./pages/Word";
 
 function App() {
+  const { height, width } = useWindowDimensions();
+  const isMobile = width <= 768;
+
+  if (isMobile) {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/home"
+            element={
+              <Protected>
+                <Home />
+              </Protected>
+            }
+          />
+          <Route path="/celebration" element={<Celebration />} />
+          <Route path="/word" element={<Word />} />
+        </Routes>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/home"
-          element={
-            <Protected>
-              <Home />
-            </Protected>
-          }
-        />
-        <Route path="/celebration" element={<Celebration />} />
-        <Route path="/word" element={<Word />} />
-      </Routes>
+    <div>
+      Please open this link on a mobile device for the complete experience!
     </div>
   );
 }
