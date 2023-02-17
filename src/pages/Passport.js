02151c4ec26d546dc62002637d3279be2af59c55
Ayush Jain <ayushjain2205@gmail.com>
2023-02-17 21:18:57 +0530

    Add passport page

--- src/pages/Passport.js
+++ src/pages/Passport.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Passport = () => {
   return (
-    <div>
-      <h1>Passport</h1>
+    <div className="relative bg-white w-full h-[665px] overflow-hidden text-center text-4xl text-black font-work-sans">
+      <div className="absolute top-[64px] left-[80px] w-[233px] h-12">
+        <div className="absolute text-[32px] top-[0px] left-[0px] tracking-[0.2px] leading-[32px] flex items-center justify-center w-[233px] h-12">
+          YOUR STAMPS
+        </div>
+      </div>
+      <Link to="/passportview">
+        <img
+          className="absolute top-[134px] left-[54px] w-[286px] h-[397.98px]"
+          alt=""
+          src="../passport.svg"
+        />
+      </Link>
+
+      <img
+        className="absolute top-[14px] left-[349px] w-5 h-5"
+        alt=""
+        src="../menu.svg"
+      />
     </div>
   );
 };
