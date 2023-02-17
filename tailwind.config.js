5e7846f6a2864fa45ace3b92d219d4edd136b2bd
Ayush Jain <ayushjain2205@gmail.com>
2023-02-17 14:51:38 +0530

    Change config of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fff",
+        gray: { 100: "#262626", 200: "rgba(0, 0, 0, 0.54)" },
+        orangered: "#f24e1e",
+        black: "#000",
+        goldenrod: "#ffc022",
+      },
+      fontFamily: { "work-sans": "'Work Sans'" },
+    },
+    fontSize: { xs: "12px", sm: "16px", base: "20px", lg: "24px" },
   },
-  plugins: [],
+  corePlugins: { preflight: false },
 };
