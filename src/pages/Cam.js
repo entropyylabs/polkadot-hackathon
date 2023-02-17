c3cb0c701f66d4b8079d4fa33ba23fb459ddbc37
Ayush Jain <ayushjain2205@gmail.com>
2023-02-17 21:24:43 +0530

    Add delay for AR view

--- src/pages/Cam.js
+++ src/pages/Cam.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Cameraview from "../components/Cameraview";
 
 const Cam = () => {
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowImage(true);
+    }, 7000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
   return (
     <div>
       <div style={{ zIndex: 10000000, position: "absolute" }}>
         <Link to="/celebration">
-          <img
-            className="fixed top-[250px] left-[100px] h-[202px] w-[207px]"
-            alt=""
-            src="../mickey.png"
-          />
+          {showImage && (
+            <img
+              className="fixed top-[250px] left-[100px] h-[202px] w-[207px]"
+              alt=""
+              src="../mickey.png"
+            />
+          )}
         </Link>
       </div>
       <div style={{ zIndex: 0 }}>
